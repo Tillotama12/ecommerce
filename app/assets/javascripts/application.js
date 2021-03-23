63046fb7ca4f686741bbe5c30a71ac4795938731
unknown <tanushrichoukhunde@gmail.comgit config --global user.name Tillotama12>
2021-03-24 00:59:50 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,225 @@
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require bootstrap
+//= require jquery.nicescroll
+
+// $(".add-to-basket").click(function(){
+//   var productId = $(this).attr("productID");
+//   $.post('/line_items_controller', {id: productId}).done(function(response){
+//       if (response.status == "created"){
+//        $('total_cart_items_badge').text(response.line_items_count);
+//       }
+//       else{
+//          //your error handler
+//       }
+//   });
+// });
+
+
+
+$(document).ready(function()
+{
+    /* smooth scrolling for scroll to top */
+	$('#to-top').bind('click', function()
+	{
+		$('body,html').animate({
+			scrollTop: 0},
+			2500);
+	});
+
+	//Easing Scroll replace Anchor name in URL and Offset Position
+	$(function(){
+		$('a[href*=#]:not([href=#])').click(function()
+		{
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top -420
+					}, 3500, 'easeOutBounce');
+					return false;
+				}
+			}
+		});
+	});
+});
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+});
+
+
+$(document).ready(function()
+{
+
+	// Closes the sidebar menu on menu-close button click event
+	$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+																// - will not work otherwise")
+		$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+		e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+		/*!
+		======================= Notes ===============================
+		* see: .sidebar-wrapper.active in: style.css
+		==================== END Notes ==============================
+		*/
+	});															//Close 'function()'
+
+	// Open the Sidebar-wrapper on Hover
+	$("#menu-toggle").hover(function(e)							//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});
+
+	$("#menu-toggle").bind('click',function(e)					//declare the element event ...'(e)' = event (shorthand)
+	{
+		$("#sidebar-wrapper").toggleClass("active",true);		//instead on click event toggle active CSS element
+		e.preventDefault();										//prevent the default action ("Do not remove as the code
+	});															//Close 'function()'
+
+	$('#sidebar-wrapper').mouseleave(function(e)				//declare the jQuery: mouseleave() event
+																// - see: ('//api.jquery.com/mouseleave/' for details)
+	{
+		/*! .toggleClass( className, state ) */
+		$('#sidebar-wrapper').toggleClass('active',false);		/* toggleClass: Add or remove one or more classes from each element
+																in the set of matched elements, depending on either the class's
+																presence or the value of the state argument */
+		e.stopPropagation();									//Prevents the event from bubbling up the DOM tree
+																// - see: ('//api.jquery.com/event.stopPropagation/' for details)
+
+		e.preventDefault();										// Prevent the default action of the event will not be triggered
+																// - see: ('//api.jquery.com/event.preventDefault/' for details)
+	});
+});
+// Closes the sidebar menu on menu-close button click event
+$("#menu-close").click(function(e)							//declare the element event ...'(e)' = event (shorthand)
+{
+															// - will not work otherwise")
+	$("#sidebar-wrapper").toggleClass("active");			//instead on click event toggle active CSS element
+	e.preventDefault(); 									//prevent the default action ("Do not remove as the code
+
+	/*!
+	======================= Notes ===============================
+	* see: .sidebar-wrapper.active in: style.css
+	==================== END Notes ==============================
+	*/
+});															//Close 'function()'
+
+
+// Anchor URL Rewrite
+/* $(document).ready(function()
+{
+	function filterPath(string)
+	{
+		return string.replace(/^\//,'').replace(/(index|default).[a-zA-Z]{3,4}$/,'').replace(/\/$/,'');
+	}
+
+	var locationPath = filterPath(location.pathname);
+	var scrollElem = scrollableElement('html', 'body');
+
+	$('a[href*=#]').each(function()
+	{
+		var thisPath = filterPath(this.pathname) || locationPath;
+		if (  locationPath == thisPath
+		&& (location.hostname == this.hostname || !this.hostname)
+		&& this.hash.replace(/#/,''))
+		{
+			var $target = $(this.hash), target = this.hash;
+			if (target)
+			{
+				var targetOffset = $target.offset().top -420;
+				$(this).bind('click',function(event)
+				{
+					$(scrollElem).animate({scrollTop: targetOffset}, 1500,'easeOutBounce');
+					event.preventDefault();
+				});
+			}
+		}
+	});
+
+	// use the first element that is "scrollable"
+	function scrollableElement(els)
+	{
+		for (var i = 0, argLength = arguments.length; i <argLength; i++)
+		{
+			var el = arguments[i],
+			$scrollElement = $(el);
+			if ($scrollElement.scrollTop()> 0)
+			{
+				return el;
+			}
+
+			else
+			{
+				$scrollElement.scrollTop(1);
+				var isScrollable = $scrollElement.scrollTop()> 0;
+				$scrollElement.scrollTop(0);
+				if (isScrollable)
+				{
+					return el;
+				}
+			}
+		}
+
+		return [];
+	}
+});*/
+
+// Anchor URL Rewrite
+/*$(function()
+{
+	$('a[href*=#]:not([href=#])').bind('click', function(evt)
+	{
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname)
+		{
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length)
+			{
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1500, 'easeInOutExpo');//jQuery easing required for animation effects
+				evt.preventDefault();
+			}
+		}
+	});
+});*/
+
+/*!
+
+
+Simply:
+
+	a[href*=#]
+
+		* get all anchors (a) that contains # in href but with:
+
+	:not([href=#])
+
+		* exclude anchors with href exaclty equals to #
+
+example:
+
+	<a href="#step1">yes</a>
+	<a href="page.php#step2">yes</a>
+	<a href="#">no</a>
+
+		* the selectors get first two anchor but it exclude the last
+		source: http://stackoverflow.com/questions/20947529/what-does-ahref-nothref-code-mean
+
+*/
